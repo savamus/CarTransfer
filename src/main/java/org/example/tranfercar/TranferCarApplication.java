6dcdaf200670c077f827d05a94759bd10d7957b1
Saveli Mussijenko <samuss@ttu.ee>
2025-07-20 10:33:15 +0300

    Initial commit

---
+++ src/main/java/org/example/tranfercar/TranferCarApplication.java
@@ -0,0 +1,13 @@
+package org.example.tranfercar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranferCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranferCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tranfercar/TranferCarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tranfercar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranferCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
